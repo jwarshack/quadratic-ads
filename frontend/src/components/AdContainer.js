791c30a4cd3404ac6b73d4a1467f7748848c9228
Jake Warren <jakewarren@Jakes-MBP-2.lan>
2021-11-29 10:33:54 -0800

    began on smart contract

--- frontend/src/components/AdContainer.js
+++ frontend/src/components/AdContainer.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Box, Image, Text} from "@chakra-ui/react"
+import { VStack, Image, Button, Text} from "@chakra-ui/react"
 import ad from '../7up.jpg'
 
 export default function AdContainer() {
     return (
-        <Box>
-            <Image src={ad} h={500}></Image>            
-        </Box>
+        <VStack spacing={5}>
+            <Image src={ad} h={500}></Image>
+            <Text>Current Lifetime:</Text>
+            <Button>Extend Lifetime</Button>       
+        </VStack>
     )
 }
